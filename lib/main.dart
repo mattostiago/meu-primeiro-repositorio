545836043951be8a6596f1bef1fe58a2391f25ea
Tiago Mattos <tiagomattos2@gmail.com>
2021-05-24 16:27:37 -0300

    Ataulização já com o github vinculado ao git

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Alterando para teste - branch test',
+      //ativando o teste com github online
+      title: 'Alterando para teste - branch test4',
       theme: ThemeData(
         //
         // Try running your application with "flutter run". You'll see the
